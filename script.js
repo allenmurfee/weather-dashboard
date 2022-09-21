76c0f51c0a3d5b6f847a75e01ea7d102093fb352
allenmurfee <allenmurfee123@gmail.com>
2022-09-21 08:43:03 -0400

    jumbotron current conditions working

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 var searchBox = $("#search");
 var searchButton = $("#searchButton");
 
+var currentConditions = $("#current-conditions");
+var recentSearches = $("#recent");
+
+//Hide Current conditions and recent searches on page load. Will probably need to change recent searches since it should load if there's local storage
+currentConditions.hide();
+recentSearches.hide();
+
 //Functions
 function getInfo(city) {
   var city = searchBox.val();
@@ -29,22 +36,44 @@ function getInfo(city) {
     .catch(function (error) {
       alert("Unable to gather data.");
     });
-  getWeather(city);
 }
 
-function getWeather(geoData) {
+function getWeather(data) {
+  var lon = data[0].lon;
+  var lat = data[0].lat;
+
   var apiKey = "5e5caac541b127539886bcfa14cf538f";
-  var apiUrl =
+  var forecastApiUrl =
     "https://api.openweathermap.org/data/2.5/forecast?lat=" +
     lat +
     "&lon=" +
-    lat +
+    lon +
     "&appid=" +
-    apiKey;
+    apiKey +
+    "&units=imperial";
 
-  var lon = geoData[0].lon;
-  var lat = geoData[0].lat;
-  console.log(lon);
+  fetch(forecastApiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayWeather(data);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to gather data.");
+    });
+}
+
+function displayWeather(data) {
+  currentConditions.show();
+  console.log(data);
+  $("#city-name").text(searchBox.val());
+  $("#temp").text("Temperature: " + data.list[0].main.temp);
+  $("#wind").text("Wind: " + data.list[0].wind.speed);
+  $("#humidity").text("Humidity: " + data.list[0].main.humidity);
 }
 
 //Click Events
